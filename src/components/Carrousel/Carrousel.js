1c9bb1e3ccf76c1444aa348617551cf3a1910ef8
William Torrejon Celis <w.torrejon10@gmail.com>
2023-10-09 22:12:44 -0500

    add Carrousel

---
+++ src/components/Carrousel/Carrousel.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from 'reactstrap';
+
+const items = [
+  {
+    src: 'https://superfanzone.com/modules/angarslider/views/img/images/795cdf25114bf6b13d8cb7406fce9515fcd4d701_65b7f193-99c0-472a-a76a-61e794ec996e.jpg',
+    altText: 'Slide 1',
+    caption: 'Slide 1',
+    key: 1,
+  },
+  {
+    src: 'https://superfanzone.com/modules/angarslider/views/img/images/b40047e446eb659effc756e9d855a296bff544d9_Banner%20Novedades-5.jpg',
+    altText: 'Slide 2',
+    caption: 'Slide 2',
+    key: 2,
+  },
+  {
+    src: 'https://superfanzone.com/modules/angarslider/views/img/images/b3f635f1bf6d7e4821cdbd7c441465a70ee52cbd_Banner%20Tazas.jpg',
+    altText: 'Slide 3',
+    caption: 'Slide 3',
+    key: 3,
+  },
+];
+
+function Carouselozaru (args) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  };
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  };
+
+  const slides = items.map((item) => {
+    return (
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+        {/* <CarouselCaption
+          captionText={item.caption}
+          captionHeader={item.caption}
+        /> */}
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+      {...args}
+    >
+      <CarouselIndicators
+        items={items}
+        activeIndex={activeIndex}
+        onClickHandler={goToIndex}
+      />
+      {slides}
+      <CarouselControl
+        direction="prev"
+        directionText="Previous"
+        onClickHandler={previous}
+      />
+      <CarouselControl
+        direction="next"
+        directionText="Next"
+        onClickHandler={next}
+      />
+    </Carousel>
+  );
+}
+
+export default Carouselozaru;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,40 +1,39 @@
 import React from 'react'
 import { Link, NavLink } from "react-router-dom";
 import Logo from '../Logo/Logo'
+import CartWidget from '../CartWidget/CartWidget';
+import './Navbar.css'
+import '../../'
 
 const Navbar = () => {
-  return (
-    <div className='navbar-container' id='navbar'>
-        <div className="row">
-                <div className="col-6">
-                    <nav className="navbar navbar-expand-lg ">
-                        <div className="container-fluid">
-                            <Link className="navbar-brand" to={"/"}><Logo /></Link>
-                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
-                            <div className="collapse navbar-collapse" id="navbarNav">
-                                <ul className="navbar-nav">
-                                    <li className="nav-item">
-                                        <NavLink className="nav-link" aria-current="page" to={"/category/funko"} >Celulares</NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                        <NavLink className="nav-link" to={"/category/figuras"}>Notebooks</NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                        <NavLink className="nav-link" to={"//category/cards"}>Auriculares</NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="justify-content-end  align-items-center" to={"/cart"}><CartWidget /></Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </nav>
+    return (
+
+        <nav className="navbar navbar-expand-lg ">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to={"/"}><Logo /></Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" aria-current="page" to={"/category/funko"} >Funko</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to={"/category/figuras"}>Figuras</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to={"/category/cards"}>Cards</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="justify-content-end  align-items-center" to={"/cart"}><CartWidget /></Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
-    </div>
-  )
+        </nav>
+
+    )
 }
 
 export default Navbar
