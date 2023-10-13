8246421a59b683cbd58de6f96fe83891abc2e7eb
William Torrejon Celis <w.torrejon10@gmail.com>
2023-10-13 00:54:20 -0500

    Modify ItemCart.css, ItemCart.js

--- src/components/ItemCart/ItemCart.js
+++ src/components/ItemCart/ItemCart.js
@@ -7,31 +7,6 @@ const ItemCart = ({ product }) => {
     const { removeProduct } = useCartContext();
     return (
         <div className='itemCart'>
-            <div>
-                <h1>Carrito de Compras</h1>
-            </div>
-            <div className='wrapper-tittlesProducts'>
-                <ul className='tittlesProducts'>
-                    <li>
-                        Imagen
-                    </li>
-                    <li>
-                        Descripcion
-                    </li>
-                    <li>
-                        Cantidad
-                    </li>
-                    <li>
-                        P/unitario
-                    </li>
-                    <li>
-                        Subtotal
-                    </li>
-                    <li>
-                        Eliminar
-                    </li>
-                </ul>
-            </div>
             <div className='wrapper-productList'>
                 <picture>
                     <img className='ImgProductList' src={product.img} alt={product.title} />
@@ -39,14 +14,16 @@ const ItemCart = ({ product }) => {
                 <div className='nameProductList'>
                     <p>{product.name}</p>
                 </div>
-            </div>
-
-            <div>
-                <p>Título: {product.name}</p>
-                <p>Cantidad: {product.quantity}</p>
-                <p>Precio u.: $ {product.price}</p>
-                <p>Subtotal: ${product.quantity * product.price}</p>
-                <button onClick={() => removeProduct(product.id)}>
+                <div className='quantityProductList'>
+                    <p>{product.quantity}</p>
+                </div>
+                <div className='priceProductList'>
+                    <p>${product.price}</p>
+                </div>
+                <div className='multProductList'>
+                    <p>${product.quantity * product.price}</p>
+                </div>
+                <button className='btnRemove' onClick={() => removeProduct(product.id)}>
                     <span class="material-symbols-outlined">
                         delete
                     </span>
